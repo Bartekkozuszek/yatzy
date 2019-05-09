1cb9a8fe17d49e0299283613df7e4cfa8d9f87df
Bartek Kozuszek <bartekkozuszek@gmail.com>
2019-05-09 17:58:27 +0200

    the first commit

---
+++ src/game/Game.js
@@ -0,0 +1,183 @@
+import React, {Component} from 'react';
+import {Table} from './Table';
+import {DiceTray} from './Dice';
+import {getScoringCategories, getCategoryScore} from './Scoring';
+import {Player} from './Player';
+
+export class Game extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			players: createPlayers(this.props.playerNames),
+			currentPlayer: 0,
+			rollsLeft: 3,
+			dice: [0, 0, 0, 0, 0],
+			heldDice: [false, false, false, false, false],
+			rollingDice: null,
+			rollingDiceTumbleCounts: null,
+		};
+	}
+
+	beginRoll() {
+		if (this.state.rollsLeft === 0) {
+			// No more rolls left; just return
+			return;
+		}
+
+		const tumbleCounts = this.state.heldDice.map(held =>
+			held ? 0 : getTumbleCount()
+		);
+		this.setState({
+			...this.state,
+			rollsLeft: this.state.rollsLeft - 1,
+			rollingDice: this.state.dice,
+			rollingDiceTumbleCounts: tumbleCounts,
+		});
+
+		this.timeTumble();
+	}
+
+	timeTumble() {
+		window.setTimeout(() => {
+			this.tumbleDice();
+		}, 50);
+	}
+
+	tumbleDice() {
+		if (this.state.rollingDiceTumbleCounts.every(c => c === 0)) {
+			this.endRoll();
+			return;
+		}
+
+		const rollingDice = this.state.rollingDice.map((die, i) =>
+			this.state.rollingDiceTumbleCounts[i] === 0
+				? die
+				: getDieRoll()
+		);
+		const tumbleCounts = this.state.rollingDiceTumbleCounts
+			.map(c => Math.max(0, c - 1));
+		this.setState({
+			...this.state,
+			rollingDice: rollingDice,
+			rollingDiceTumbleCounts: tumbleCounts,
+		});
+
+		this.timeTumble();
+	}
+
+	endRoll() {
+		this.setState({
+			...this.state,
+			dice: this.state.rollingDice,
+			rollingDice: null,
+			rollingDiceTumbleCounts: null,
+		});
+	}
+
+	clickDie(index) {
+		if (this.state.dice[index] === 0) {
+
+			return;
+		}
+
+		const heldDice = this.state.heldDice.slice();
+		heldDice[index] = !heldDice[index];
+		this.setState({
+			...this.state,
+			heldDice: heldDice,
+		});
+	}
+
+	clickCategory(category) {
+		if (this.state.dice[0] === 0) {
+
+			return;
+		}
+
+		const player = this.state.players[this.state.currentPlayer];
+
+		if (player.hasCategoryScore(category.id) || category.isDerived) {
+
+			return;
+		}
+
+		const score = getCategoryScore(category.id, player, this.state.dice);
+
+		const players = this.state.players.slice();
+		players[this.state.currentPlayer] = player.setCategoryScore(
+			category.id,
+			score
+		);
+
+		this.setState({
+			...this.state,
+			players: players,
+			rollsLeft: 3,
+			dice: [0, 0, 0, 0, 0],
+			heldDice: [false, false, false, false, false],
+			currentPlayer: (this.state.currentPlayer + 1) % players.length,
+		});
+	}
+
+	gameHasEnded() {
+		if (this.state.currentPlayer !== 0) {
+			return false;
+		}
+
+		const player = this.state.players[0];
+		const categories = getScoringCategories();
+		return categories.every(c => c.isDerived || player.hasCategoryScore(c.id));
+	}
+
+	render() {
+		return (
+			<div className='game-area'>
+				<Table players={this.state.players}
+					   categories={getScoringCategories()}
+					   dice={this.state.rollingDice ? null : this.state.dice}
+					   currentPlayer={this.state.currentPlayer}
+					   onClickCategory={c => this.clickCategory(c)}/>
+				<DiceTray dice={this.state.rollingDice || this.state.dice}
+				          held={this.state.heldDice}
+				          rollsLeft={this.state.rollsLeft}
+				          rolling={this.state.rollingDice ? true : false}
+				          onClickRoll={() => this.beginRoll()}
+				          onClickDie={i => this.clickDie(i)}/>
+			</div>
+		);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (this.gameHasEnded()) {
+			this.props.onGameEnd(this.state.players);
+		}
+	}
+}
+
+function createPlayers(names) {
+	const randomNames = shuffle(names);
+	return randomNames.map(name => new Player(name));
+}
+
+function shuffle(items) {
+	items = items.slice();
+	for (let i = items.length - 1; i > 0; i--) {
+		const n = Math.floor((i + 1) * Math.random());
+
+		const temp = items[i];
+		items[i] = items[n];
+		items[n] = temp;
+	}
+
+	return items;
+}
+
+function getTumbleCount() {
+
+	return 5 + Math.floor(13 * Math.random());
+}
+
+function getDieRoll() {
+	return 1 + Math.floor(6 * Math.random());
+}
---
+++ src/game/Table.js
@@ -0,0 +1,210 @@
+import React, {Component} from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {getCategoryScore} from './Scoring';
+
+export class Table extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			confirmZeroCategory: null,
+		};
+	}
+
+	handlePickCategory(category) {
+		const player = this.props.players[this.props.currentPlayer];
+
+		let needConfirmation = false;
+		if (this.props.dice[0] !== 0) {
+			needConfirmation =
+				!player.hasCategoryScore(category.id) &&
+				getCategoryScore(category.id, player, this.props.dice) === 0 &&
+				this.state.confirmZeroCategory !== category.id;
+		}
+
+		if (needConfirmation) {
+			this.setState({
+				...this.state,
+				confirmZeroCategory: category.id,
+			});
+		} else {
+			// Clear any pending timeout
+			this.removeConfirmZero();
+			this.props.onClickCategory(category);
+		}
+	}
+
+	removeConfirmZero() {
+		this.setState({
+			...this.state,
+			confirmZeroCategory: null,
+		});
+	}
+
+	render() {
+		const players = this.props.players;
+		const confirmZeroCategory = this.state.confirmZeroCategory;
+
+		const playerNames = players.map((p, i) =>
+			<th key={i}
+			    className={i === this.props.currentPlayer ? 'player-current' : ''}>
+				{p.name}
+			</th>
+		);
+
+		const categories = this.props.categories.map(cat =>
+			<BoardRow key={cat.id}
+			          category={cat}
+			          players={players}
+			          currentPlayer={this.props.currentPlayer}
+			          dice={this.props.dice}
+			          confirmZero={cat.id === confirmZeroCategory}
+			          onPickCategory={() => this.handlePickCategory(cat)}/>
+		);
+
+		const totalScores = players.map((p, i) =>
+			<td key={i}>{p.getTotalScore()}</td>
+		);
+
+		return (
+			<table className='game-board'>
+				<thead>
+					<tr>
+						<th></th>
+						{playerNames}
+					</tr>
+				</thead>
+				<tbody>
+					{categories}
+				</tbody>
+				<tfoot>
+					<tr className='category--derived category--total'>
+						<th>Total</th>
+						{totalScores}
+					</tr>
+				</tfoot>
+			</table>
+		);
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		if (prevState.confirmZeroCategory !== this.state.confirmZeroCategory) {
+
+			if (prevState.confirmZeroCategory !== null) {
+				window.clearTimeout(this.confirmZeroTimeoutId);
+			}
+
+
+			if (this.state.confirmZeroCategory !== null) {
+				this.confirmZeroTimeoutId = window.setTimeout(() => {
+					this.removeConfirmZero();
+				}, 2500);
+			}
+		}
+	}
+}
+
+export function BoardRow(props) {
+	const category = props.category;
+	const players = props.players.map((p, i) => {
+		const isCurrent = i === props.currentPlayer;
+		return (
+			<PlayerScore key={i}
+			             player={p}
+			             isCurrent={isCurrent}
+			             category={category}
+			             confirmZero={props.confirmZero && isCurrent}
+			             dice={isCurrent ? props.dice : null}
+			             onPickCategory={isCurrent ? (() => props.onPickCategory()) : null}/>
+		);
+	});
+
+	const className = category.isDerived
+		? `category--derived category--${category.id}`
+		: `category--${category.id}`;
+
+	return (
+		<tr className={className}
+		    onClick={() => props.onPickCategory()}>
+			<th>{category.displayName}</th>
+			{players}
+		</tr>
+	);
+}
+
+export function PlayerScore(props) {
+	const dice = props.dice;
+	const player = props.player;
+	const category = props.category;
+	const canScore =
+		!category.isDerived &&
+		!player.hasCategoryScore(category.id) &&
+		props.isCurrent &&
+		props.dice &&
+		props.dice[0] !== 0;
+
+	let className = 'score';
+	let text = null;
+
+	if (category.isDerived) {
+		const score = getCategoryScore(category.id, player, dice);
+		text = score;
+		if (score === 0) {
+			className += ' score--zero';
+		}
+	} else if (player.hasCategoryScore(category.id)) {
+		const score = player.getCategoryScore(category.id);
+		if (score !== 0) {
+			text = score;
+		} else {
+			className += ' score--zero';
+			text = '\u2014';
+		}
+	} else if (dice && dice[0] !== 0) {
+		const score = getCategoryScore(category.id, player, dice);
+		text = score;
+		className += ' score--new';
+		if (score === 0) {
+			className += ' score--zero';
+		}
+	}
+
+	if (props.isCurrent) {
+		className += ' player-current';
+	}
+
+	return (
+		<td className={className}
+		    tabIndex={canScore ? 0 : null}
+		     onKeyDown={canScore ? (e => activateFromKeyboard(e, props.onPickCategory)) : null}>
+			{text}
+			<ReactCSSTransitionGroup
+					transitionName='confirm-zero'
+					transitionEnterTimeout={50}
+					transitionLeaveTimeout={300}>
+				{props.confirmZero && <ConfirmZeroPopup/>}
+			</ReactCSSTransitionGroup>
+		</td>
+	);
+}
+
+export function ConfirmZeroPopup(props) {
+	return (
+		<div className='confirm-zero-popup'>
+			Once more to<br/>
+			confirm <strong>0</strong> points
+		</div>
+	);
+}
+
+function activateFromKeyboard(e, callback) {
+	const keyCode = e.which || e.keyCode;
+	switch (keyCode) {
+		case 13:
+		case 32:
+			callback();
+			return true;
+		default:
+			return false;
+	}
+}
